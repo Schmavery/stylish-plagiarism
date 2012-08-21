fa3d557fb731f96e509370e25660286ee7e61288
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2012-08-21 17:42:41 -0400

    Some stuff

--- thesaurus.py
+++ thesaurus.py
@@ -7,7 +7,7 @@ class MainPage(webapp2.RequestHandler):
             <body>
               <h1>The Stylish Plagiarizer</h1>
               <form action="/results" method="post">
-                <div><input name="number" ></div>
+                <div><input name="textbox" ></div>
                 <div><input type="submit" value="Calculate"></div>
               </form>
             </body>
@@ -15,23 +15,61 @@ class MainPage(webapp2.RequestHandler):
 
 class Compute(webapp2.RequestHandler):
     def post(self):
-        x = self.request.get('number')
-        x = int(x)
-        list = []
-        factors = []
-        for i in range(1,(x+1)):
-            list.append(i)
-            
-        for n in range(0,x):
-            for m in range(0,x):
-                
-                ans = list[m] * list[n]
-                if ans == x:
-                    factors.append(list[m])
+        
+        thesFile = open("thesaurus.txt")
 
-        self.response.out.write("""
-            <p>The factors of x are:<br>""")
-        self.response.out.write(factors)
+        sampleText = "Hunter fared poorly at the box office at the time of its release, making only $8.6 million in the United States."
+
+        ##Load thesaurus into dictionary with synonyms as tuples##
+
+        thes = {}
+            
+        for line in thesFile:
+            #print line
+            words = string.split(line)
+            tempSynonyms = []
+            firstWord = words[0] 
+            for word in words:
+                if (not word == firstWord):
+                    tempSynonyms.append(word)
+            tempSynonyms = tuple(tempSynonyms)
+            thes[firstWord] = tempSynonyms
+
+
+        ##Create and display altered text##
+        running = True
+        while running:
+
+            sampleText = self.request.get('textbox')
+
+            if (sampleText == 'q'):
+                self.response.out.write("Thank you for using the Plagiariser.")
+                running = False;
+            else:
+                finalList = []
+                sampleList = string.split(sampleText)
+                for word in sampleList:
+                    if (word in thes):
+                        finalList.append(thes[word][random.randint(0,len(thes[word]) - 1)])
+                    else:
+                        finalList.append(word)
+                finalText = string.join(finalList, " ")
+                
+                self.response.out.write("\n\nPlagiarised:")
+                self.response.out.write(finalText)
+
+                totalChanged = 0
+                ##check "security" of "translation"##
+                for i, word in enumerate(sampleList):
+                    if (not word == finalList[i]):
+                        totalChanged += 1
+
+                self.response.out.write(str(totalChanged) + "/" + str(len(sampleList)))
+                percentage = 100*(float(totalChanged) / len(sampleList))
+                percentage = min(100, percentage + 40)
+
+                self.response.out.write("Safety score:" + '%.2f' % (percentage) + "%")
+                self.response.out.write("=================================")
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
@@ -43,7 +81,7 @@ app = webapp2.WSGIApplication([('/', MainPage),
 
 
 
-
+##################################################################################################################################
 
 
 
